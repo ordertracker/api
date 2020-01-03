f2294a5c7f81dc80b91bfa0d20789b8df3d5aad5
Ilche Bedelovski <ilche.bedelovski@gmail.com>
2020-01-03 16:59:20 +0100

    Add relationship between users and roles

--- app/helpers/bootstrap.py
+++ app/helpers/bootstrap.py
@@ -5,18 +5,19 @@ from app.models.user import UserModel
 from app.models.role import RoleModel
 
 '''
-Create default user in database
+Create default admin user in database
 '''
 def default_user():
     
     username = app.config.get('DEFAULT_USERNAME') 
     password = app.config.get('DEFAULT_PASSWORD') 
     name = app.config.get('DEFAULT_NAME') 
-    email = app.config.get('DEFAULT_EMAIL') 
+    email = app.config.get('DEFAULT_EMAIL')
+    role_id = 1
 
     user = UserModel.find_user_by_username(username)
     if not user:
-        user = UserModel(username, hashlib.sha256(password.encode("utf-8")).hexdigest(), name, email)
+        user = UserModel(username, hashlib.sha256(password.encode("utf-8")).hexdigest(), name, email, role_id)
         user.save_to_db()
         app.logger.info('User %s created successfully', username)
 
